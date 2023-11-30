eaca98f73f1d22446ece86eee33e22b2c3cb1dea
NachoConort <nachoconort@gmail.com>
2023-11-30 14:04:53 -0300

    Agregada parte de la evaluacion de la posicion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const columnas = document.querySelectorAll(".columna");
 
 let turno = 0;
+let posicion = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 for(let i = 0; i < columnas.length; i++) {
   columnas[i].setAttribute("id", "" + (i+1));
@@ -8,15 +9,19 @@ for(let i = 0; i < columnas.length; i++) {
 
 columnas.forEach(columna => {
   columna.addEventListener("click", () => {
+    let todasLasCeldas = document.querySelectorAll(".celda");
     const celdas = document.querySelectorAll(".c" + columna.id);
     let ficha = crearFicha();
-    let fichaNueva = document.getElementsByClassName("ficha");
-    celdas.forEach(celda => {
-      if(!celda.hasChildNodes()) {
-        celda.appendChild(ficha)
+    for(let i = celdas.length -1; i >= 0; i--) {
+      if(!celdas[i].hasChildNodes()) {
+        celdas[i].appendChild(ficha);
+        turno++;
+        break;
       }
-    })
-    turno++;
+    };
+    actualizarPosicion(todasLasCeldas);
+    console.log(posicion);
+    evaluacionPosicion();
   });
 })
 
@@ -25,11 +30,44 @@ function crearFicha () {
   ficha.style.width = "80px";
   ficha.style.height = "80px";
   ficha.style.borderRadius = "50%";
-  ficha.classList.add("ficha");
+  ficha.style.animation = "caida linear 0.5s";
   if (turno % 2 === 0) {
     ficha.style.backgroundColor = "#ffff66";
+    ficha.classList.add("a");
   } else {
     ficha.style.backgroundColor = "#ff3333";
+    ficha.classList.add("r");
   };
   return ficha;
+}
+
+function actualizarPosicion(todasLasCeldas) {
+  for(let i = 0; i < posicion.length; i++) {
+    if (todasLasCeldas[i].hasChildNodes() && todasLasCeldas[i].firstElementChild.classList.contains("a")) {
+      posicion[i] = 1;
+    } else if (todasLasCeldas[i].hasChildNodes() && todasLasCeldas[i].firstElementChild.classList.contains("r")) {
+      posicion[i] = 2;  
+    }
+  }
+}
+
+function evaluacionPosicion () {
+  for(let i = 0; i < posicion.length; i++) {
+    if(posicion[i] === 1 && posicion[i+6] === 1 && posicion[i+12] === 1 && posicion[i+18] === 1) {
+      setTimeout(() => {
+        alert("Amarillo gana!");
+      }, 600);
+      break;
+    } else if (posicion[i] === 1 && posicion[i+5] === 1 && posicion[i+10] === 1 && posicion[i+15] === 1) {
+      setTimeout(() => {
+        alert("Amarillo gana!");
+      }, 600);
+      break;
+    } else if(posicion[i] === 1 && posicion[i+7] === 1 && posicion[i+14] === 1 && posicion[i+21] === 1) {
+      setTimeout(() => {
+        alert("Amarillo gana!");
+      }, 600);
+      break;
+    }
+  }
 }
